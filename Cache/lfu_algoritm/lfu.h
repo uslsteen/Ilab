896f4a0a7cb5f37fb64784a63b0c9e2edc8f7f8b
uslsteen <shurygin0171@gmail.com>
2020-09-13 21:11:37 +0300

    baby lfu

---
+++ Cache/lfu_algoritm/lfu.h
@@ -0,0 +1,136 @@
+//
+// Created by anon on 13.09.2020.
+//
+#ifndef __LFU_H__
+#define __LFU_H__
+
+#include <iostream>
+#include <cassert>
+#include <list>
+#include <unordered_map>
+#include <vector>
+
+enum MODE
+{
+    DEBUG = 1,
+    RELEASE = 0
+};
+
+
+#define MODE DEBUG
+
+struct Cache_item
+{
+    int elem; // it should be template type
+    int freq;  // param of frequency calls cache_item
+};
+
+
+//template <typename T, typename KeyT = int>
+class Cache_t
+{
+
+private:
+    std::list<Cache_item> cache_;
+
+public:
+    size_t cap;
+
+    // typedef for iterators of map and list
+
+    typedef std::list<Cache_item>::iterator ListIt;
+    typedef std::unordered_map<int, ListIt>::iterator HashIt;
+
+    // structure map, which connect value_type (should be template) and iterator on list
+    std::unordered_map<int, ListIt> hash_;
+
+public:
+
+    Cache_t(size_t size) : cap(size)
+    {}
+
+    ~Cache_t()
+    {
+        cap = 0;
+    }
+
+    bool Look_up(const int elem);
+
+    auto Find_min_used();
+
+    bool Delete_from_cache(ListIt& min_used_elem);
+
+    struct Cache_item Cache_item_construct(int elem);
+};
+
+
+auto Cache_t::Find_min_used()
+{
+    ListIt iter = cache_.begin();
+    ListIt min_iter = iter;
+    int min_freq = iter->freq;
+
+    for (; iter != cache_.end(); ++iter)
+    {
+        if (iter->freq < min_freq)
+            min_iter = iter;
+    }
+
+    return min_iter;
+}
+
+bool Cache_t::Delete_from_cache(ListIt& min_used_elem)
+{
+    cache_.erase(min_used_elem);
+
+    return true;
+}
+
+
+bool Cache_t::Look_up(const int elem)
+{
+    HashIt hit = hash_.find(elem);
+
+    //if hit doesnt exist in map or freq == 0 (it means that elem existed in list in past)
+    if (hit == hash_.end() || (hit->second->freq == 0))
+    {
+        if (cache_.size() == cap)
+        {
+            ListIt min_used_elem = Find_min_used();
+
+            if (!Delete_from_cache(min_used_elem))
+            {
+                printf("Error in deleting from cache!\n");
+                abort();
+            }
+            //Delete_from_cache();
+
+            hit->second->freq = 0;
+        }
+
+        struct Cache_item new_item = {elem, 1};
+
+        cache_.push_front(new_item);
+        hash_[elem] = cache_.begin();
+
+#if MODE == DEBUG
+
+        ListIt iter = cache_.begin();
+        std::cout << "Elem = " << iter->elem << " Freq = " << iter->freq << "\n";
+#endif
+
+        return false;
+    }
+    //else if hit exist in map =>
+
+    hit->second->freq++;
+
+#if MODE == DEBUG
+
+    std::cout << "Elem = " << hit->second->elem << " Freq = " << hit->second->freq << "\n";
+#endif
+
+    return true;
+}
+
+#endif //__LFU_H__
---
+++ Cache/lfu_algoritm/main.cpp
@@ -0,0 +1,35 @@
+#include "lfu.h"
+
+int main()
+{
+    int hits = 0;
+    int capacity, num_of_calls = 0;
+
+    std::cout << "Insert capacity of cache:" << std::endl;
+    std::cin >> capacity;
+
+    std::cout << "Insert num of calls:" << std::endl;
+    std::cin >> num_of_calls;
+
+    Cache_t cache(capacity);
+
+    for (int i = 0; i < num_of_calls; ++i)
+    {
+        int page = 0;
+        std::cin >> page;
+
+        assert(std::cin.good());
+
+        if (cache.Look_up(page))
+            hits += 1;
+    }
+
+    std::cout << "Hits = " << hits << std::endl;
+}
+
+
+
+
+
+
+
