1c7c76915f459722eef2bec8ef38056d4246e0c3
uslsteen <shurygin0171@gmail.com>
2020-11-03 16:57:55 +0300

    i have started developing

---
+++ Second year/Triangles_Problem/Octotree/Bounding_region.h
@@ -0,0 +1,19 @@
+//
+// Created by anon on 03.11.2020.
+//
+
+#ifndef TRIANGLES_UPD_BOUNDING_REGION_H
+#define TRIANGLES_UPD_BOUNDING_REGION_H
+
+#include "
+
+class Bnding_region
+{
+private:
+
+    vec bot, top;
+
+    
+};
+
+#endif //TRIANGLES_UPD_BOUNDING_REGION_H
