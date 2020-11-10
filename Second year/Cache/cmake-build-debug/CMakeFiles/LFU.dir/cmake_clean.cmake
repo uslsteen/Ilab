716fa3026aeffb07b70bc8b7750ae11f8166b01b
uslsteen <shurygin0171@gmail.com>
2020-11-10 23:03:23 +0300

    delete useful things

---
+++ Second year/Cache/cmake-build-debug/CMakeFiles/LFU.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/LFU.dir/main.cpp.o"
+  "LFU"
+  "LFU.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/LFU.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ Second year/Cache/cmake-build-debug/CMakeFiles/LFU.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for LFU.
+# This may be replaced when dependencies are built.
