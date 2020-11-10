8f9797af99ba69cc56ed3ecf74f9e71e5ce149f9
uslsteen <shurygin0171@gmail.com>
2020-11-10 16:36:18 +0300

    some adds

--- Second year/Matrix/Matrix.h
+++ Second year/Matrix/Matrix.h
@@ -8,6 +8,7 @@
 #include <iostream>
 #include <vector>
 #include <cassert>
+#include <math.h>
 
 
 
@@ -19,35 +20,41 @@ namespace AdamR
         private:
 
             //! Num of rows and columns of my matrix
-            uint rows, clmns;
+            int rows, clmns;
 
             //! Perfomance of my matrix
             Data** matrix;
 
             //! using for special data type of my matrix
-
             using DataIt = typename std::vector<Data>::iterator;
+
+            const int EPSILON = 1e-6;
 
         public:
 
-            //! Constructors for class Matrix
+            Matrix() : rows(0),
+                       clmns(0),
+                       matrix(nullptr)
+            {
+            }
 
-            //! Constructor of matrix class of zeros
-            Matrix(uint rows_, uint clmns_) : rows(rows_),
+            Matrix(int rows_, int clmns_) : rows(rows_),
                                               clmns(clmns_)
             {
-                if (rows * clmns == 0)
-                    return;
+                assert(rows_*clmns_ != 0);
 
                 matrix = new Data* [rows];
                 for (size_t i = 0; i < rows; ++i)
                 {
-                    matrix = new Data [clmns];
+                    matrix[i] = new Data [clmns];
 
                     for (size_t j = 0; j < clmns; ++j)
                         matrix[i][j] = 0;
                 }
             }
+            //! Constructors for class Matrix
+
+            //! Constructor of matrix class of zeros
 
             //! Constructor of matrix class of value
             Matrix(uint rows_, uint clmns_, Data val = Data{}) : rows(rows_),
@@ -60,44 +67,47 @@ namespace AdamR
 
                 for (size_t i = 0; i < rows; ++i)
                 {
-                    matrix[i] = new Data [clmns];
+                    matrix[i] = new Data[clmns];
 
                     for (size_t j = 0; j < clmns; ++j)
                         matrix[i][j] = val;
                 }
+
             }
 
             //! Constructor class matrix of vector
-            Matrix(uint rows_, uint clmns_, Data beg, Data end) : rows(rows_),
-                                                                  clmns(clmns_)
+            Matrix(uint rows_, uint clmns_, const DataIt& beg, const DataIt& end) : rows(rows_),
+                                                                                    clmns(clmns_)
             {
-                if (rows * clmns == 0)
-                    return;
+                assert(rows_ * clmns_ != 0);
+
+                DataIt current = beg;
+                int num_of_elem = rows_*clmns_, cnter = 0;
 
                 matrix = new Data* [rows];
 
                 for (size_t i = 0; i < rows; ++i)
                 {
-                    if (beg == end)
-                        break;
+                    if (cnter == num_of_elem - 1)
+                        return;
 
-                    matrix = new Data [clmns];
+                    matrix[i] = new Data [clmns];
 
                     for (size_t j = 0; j < clmns; ++j)
                     {
-                        matrix[i][j] = beg;
-                        ++beg;
+                        matrix[i][j] = *current;
+                        ++current;
                     }
                 }
             }
 
-            Matrix eye(uint rows_, uint clmns_)
+            static Matrix eye(uint num)
             {
-                Matrix matr(rows_, clmns_);
+                Matrix matr(num, num);
 
-                for (int i = 0; i < rows_; ++i)
+                for (int i = 0; i < num; ++i)
                 {
-                    for (int j = 0; j < clmns; ++j)
+                    for (int j = 0; j < num; ++j)
                     {
                         if (i = j)
                             matr.matrix[i][j] = 1;
@@ -137,34 +147,13 @@ namespace AdamR
                 return clmns;
             }
 
-            /*
-            const Data& operator[](int i)
-            {
-                return
-            }
-             */
-
-            void dump(std::ostream& os) const
-            {
-                for (size_t i = 0; i < rows; ++i)
-                {
-                    os << "|| ";
-
-                    for (size_t j = 0; j < clmns; ++j)
-                        os << matrix[i][j] << " ";
-
-                    os << "||" << std::endl;
-                }
-            }
-
 
             //! Reloading of operators for class matrix
 
-            //! Make it more smart
             Matrix<Data>& operator =(const Matrix<Data>& rhs)
             {
                 if (rows != rhs.rows || clmns != rhs.clmns)
-                    Resize(rhs);
+                    Resize(rhs.rows, rhs.clmns);
 
                 rows = rhs.rows;
                 clmns = rhs.clmns;
@@ -266,17 +255,22 @@ namespace AdamR
                 return (*this);
             }
 
-            void Resize(const Matrix& mtr)
+
+            bool operator ==(const Matrix<Data>& mtr)
             {
-                for (size_t i = 0; i < clmns; ++i)
-                    delete[] matrix[i];
-
-                delete[] matrix;
-
-                matrix = new Data* [rows];
+                if (rows != mtr.rows && clmns != mtr.clmns)
+                    return false;
 
                 for (size_t i = 0; i < rows; ++i)
-                    matrix[i] = new Data[clmns];
+                {
+                    for (size_t j = 0; j < clmns; ++j)
+                    {
+                        if (std::abs(mtr.matrix[i][j] - matrix[i][j]) > EPSILON)
+                            return false;
+                    }
+                }
+
+                return true;
             }
 
 
@@ -287,7 +281,86 @@ namespace AdamR
                 return matrix[i];
             }
 
+
+            bool operator !=(const Matrix<Data>& mtr)
+            {
+                return !(operator==(mtr));
+            }
+
+
+    private:
+            //! Function for resing matrix
+            //! \param mtr
+            void Resize(uint rows_, uint clmns_)
+            {
+                assert(rows_ * clmns_ != 0);
+
+                for (size_t i = 0; i < clmns; ++i)
+                    delete[] matrix[i];
+
+                delete[] matrix;
+
+                matrix = new Data* [rows_];
+
+                for (size_t i = 0; i < rows_; ++i)
+                    matrix[i] = new Data[clmns_];
+            }
+
+
+
+    public:
+
+        /*
+            //! Function for trannsposing matrix
+            void transposition()
+            {
+                Resize(*this);
+
+
+            }
+            */
+
+
+            void dump(std::ostream& os) const
+            {
+                std::cout << std::endl;
+
+                for (size_t i = 0; i < rows; ++i)
+                {
+                    os << "|| ";
+
+                    for (size_t j = 0; j < clmns; ++j)
+                        os << matrix[i][j] << " ";
+
+                    os << "||" << std::endl;
+                }
+
+                std::cout << std::endl;
+            }
+
+
+
     };
+
+    template <typename Data>
+    Matrix<Data> operator +(const Matrix<Data>& lhs, const Matrix<Data> rhs)
+    {
+
+    }
+
+    template <typename Data>
+    Matrix<Data> operator -(const Matrix<Data>& lhs, const Matrix<Data> rhs)
+    {
+
+    }
+
+    template <typename Data>
+    Matrix<Data> operator *(const Matrix<Data>& lhs, const Matrix<Data>& rhs)
+    {
+
+    }
+
+
 }
 
 
--- Second year/Matrix/main.cpp
+++ Second year/Matrix/main.cpp
@@ -2,13 +2,38 @@
 
 using namespace AdamR;
 
+#define OUT std::cout
+
 int main()
 {
     //Matrix<int> mtr1 = {2,2};
 
-    Matrix<int> mtr2 = {2, 2, 1};
-    Matrix<int> mtr3 = {2, 2, 1};
-    mtr3 *= mtr2;
+    //Matrix<int> mtr2 = {2, 2, 1};
+    //Matrix<int> mtr3 = {2, 2, 1};
+    //mtr3 *= mtr2;
 
-    mtr3.dump(std::cout);
+    std::vector<int> buffer = {1, 1, 1, 2, 2, 2, 3, 3, 3};
+    Matrix<int> mtr4 = {3, 3, buffer.begin(), buffer.end()};
+
+    Matrix<int> mtr2 = {2, 2, 1};
+
+    mtr2.dump(OUT);
+    mtr4.dump(OUT);
+
+    Matrix<int> mtr5 = Matrix<int>::eye(3);
+
+    mtr5.dump(OUT);
+
+    mtr2 = mtr4;
+
+    mtr2 *= mtr2;
+
+
+
+    Matrix<int> mtr1 = {2,2};
+    mtr1.dump(OUT);
+
+    mtr2.dump(OUT);
+
+    //mtr3.dump(std::cout);
 }
