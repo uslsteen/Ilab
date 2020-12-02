9adeabcaaf84cdd73fe6ddeddfc40456ef023327
uslsteen <shurygin0171@gmail.com>
2020-12-02 21:07:01 +0300

    fixed det and upd tests

--- Second year/Matrix/files_for_test/test_gen.cpp
+++ Second year/Matrix/files_for_test/test_gen.cpp
@@ -1,4 +1,5 @@
-#include "Matrix.h"
+#include "Matrix.hpp"
+
 
 using namespace AdamR;
 
@@ -11,48 +12,56 @@ int main()
     int swp_cnter = 1;
     size_t num_of_str = 0;
 
-    std::cin >> size_of_mtr >> real_det;
+    OUT << "Input size of you matrix\n";
+    std::cin >> size_of_mtr;
+
+    OUT << "Input expected det of your matrix\n";
+    std::cin >> real_det;
+
+    double copy_det = real_det;
 
     Matrix<double> matr{size_of_mtr, size_of_mtr};
     matr = Matrix<double>::U_matr(size_of_mtr, 1);
     matr[0][0] *= real_det;
 
 
+    OUT << matr;
 
     srand(clock());
 
     for (size_t i = 0; i < size_of_mtr; ++i)
     {
         int j = rand() % (size_of_mtr - 1);
-        matr.swap_lines(i, j);
+        matr.swap_rows(i, j);
         swp_cnter = -swp_cnter;
     }
 
-    real_det *= swp_cnter;
+    copy_det *= swp_cnter;
 
     for (size_t i = 0; i < size_of_mtr/2; ++i)
     {
-        num_of_str = rand() % (size_of_mtr - 1);
-        double value = rand() % 10;
+        num_of_str = (rand() % (size_of_mtr - 1)) + 1;
+        double value = rand() % 10 + 1;
 
-        matr.mul_line_to_sclr(num_of_str, value);
-        real_det *= value;
+        matr.mul_rows_to_sclr(num_of_str, value);
+        copy_det *= value;
     }
 
-    double value = rand() % 3;
+    double value = (rand() % 3) + 1;
     value = 1/value;
     std::cout << "value = " << value << std::endl;
 
 
-    num_of_str = rand() % (size_of_mtr - 1);
-    matr.mul_line_to_sclr(num_of_str, value);
-    real_det *= value;
+    num_of_str = (rand() % (size_of_mtr - 1)) + 1;
+    matr.mul_rows_to_sclr(num_of_str, value);
+    copy_det *= value;
 
 
     matr.transposition();
-    std::cout << "Calculated matr determiannt = " << matr.detemrinant() << std::endl;
+    std::cout << "Calculated matr determiannt = " << matr.determ() << std::endl;
     std::cout << matr << std::endl;
-    std::cout << "Real det = " << real_det << std::endl;
+    std::cout << "Start det = " << real_det << std::endl;
+    std::cout << "Copy det = " << copy_det << std::endl;
 
 
     return 0;
