cfffc7562dbeb5055fc42b1c6ad580f8e367979c
uslsteen <shurygin0171@gmail.com>
2021-02-03 03:01:22 +0300

    start writing tree

---
+++ Second year/Tree_problem/main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <vector>
+#include "test.hpp"
+#include "my_time.hpp"
+
+using std::cout;
+using std::cin;
+using std::vector;
+
+int main()
+{
+    cout << " This program exist for comparing two tree realisation!\n";
+
+    
+
+    return 0;
+}
---
+++ Second year/Tree_problem/test.hpp
@@ -0,0 +1,8 @@
+//
+// Created by anon on 03.02.2021.
+//
+
+#ifndef TREE_PROBLEM_TEST_HPP
+#define TREE_PROBLEM_TEST_HPP
+
+#endif //TREE_PROBLEM_TEST_HPP
