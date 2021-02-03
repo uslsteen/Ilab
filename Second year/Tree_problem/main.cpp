87778f626711e9a82caadb566c0078ed4bffbdd2
uslsteen <shurygin0171@gmail.com>
2021-02-03 17:45:15 +0300

    ready simple vers

--- Second year/Tree_problem/main.cpp
+++ Second year/Tree_problem/main.cpp
@@ -1,17 +1,43 @@
 #include <iostream>
 #include <vector>
+#include <set>
 #include "test.hpp"
-#include "my_time.hpp"
 
-using std::cout;
-using std::cin;
-using std::vector;
 
 int main()
 {
     cout << " This program exist for comparing two tree realisation!\n";
 
-    
+    int num_of_keys = 0, num_of_qs = 0;
+
+    vector<int> keys;
+    vector<queries_t> queries;
+
+    cin >> num_of_keys;
+    keys.resize(num_of_keys);
+
+    for (int i = 0; i < num_of_keys; ++i)
+        cin >> keys[i];
+
+    cin >> num_of_qs;
+    queries.resize(num_of_qs);
+
+    for (int i = 0; i < num_of_qs; ++i)
+        cin >> queries[i].frst >> queries[i].scnd;
+
+
+    /* std::set tree test */
+    std::set<int> std_tree;
+    cout << "There is start testing of std::set!\n";
+    vector<int> res_vec = Tree_testing(std_tree, keys, queries);
+
+    for (auto res : res_vec)
+        cout << res << " ";
+
+
+    cout << "\nThere is end testing of std::set\n";
+
+    /* my tree test */
 
     return 0;
 }
--- Second year/Tree_problem/test.hpp
+++ Second year/Tree_problem/test.hpp
@@ -5,4 +5,50 @@
 #ifndef TREE_PROBLEM_TEST_HPP
 #define TREE_PROBLEM_TEST_HPP
 
+#include "my_time.hpp"
+
+using std::vector;
+using std::cin;
+using std::cout;
+using std::endl;
+
+struct queries_t
+{
+    int frst, scnd;
+};
+
+
+template <typename Tree>
+vector<int> Tree_testing(Tree& tree, vector<int>& keys, vector<queries_t>& queries)
+{
+    Time::Timer tmer;
+
+    vector<int> res(queries.size());
+
+    for (auto key : keys)
+        tree.insert(key);
+
+    for (int q_beg = 0, q_end = queries.size(); q_beg != q_end; ++q_beg)
+    {
+        auto start_it = tree.lower_bound(queries[q_beg].frst);
+        auto end_it = tree.end();
+        int cnter = 0;
+
+        if (start_it == end_it)
+            cout << "Add processing!\n";
+
+        while ((start_it != end_it) && (*start_it <= queries[q_beg].scnd))
+        {
+            ++start_it;
+            ++cnter;
+        }
+
+        res[q_beg] = cnter;
+    }
+
+    cout << "Time elapsed: " << tmer.elapsed() << " msecs" << endl;
+
+    return res;
+}
+
 #endif //TREE_PROBLEM_TEST_HPP
