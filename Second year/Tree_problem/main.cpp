e0e7e5b306cdfcb9e9590a5c2cbb9158b5c6e457
uslsteen <shurygin0171@gmail.com>
2021-02-16 00:20:28 +0300

    new targets

--- Second year/Tree_problem/main.cpp
+++ Second year/Tree_problem/main.cpp
@@ -6,9 +6,18 @@
 std::string dotname = "tree14.dot";
 std::string pngname = "tree14.png";
 
-int main()
+int main(int argc, char** argv)
 {
-    cout << " This program exist for comparing two tree realisation!\n";
+    if (argc < 2)
+    {
+        std::cerr << "Lack of the arguments!\n" << "Please, try once more!\n";
+        return 0;
+    }
+
+    opt_t option = Get_options(argc, argv);
+
+
+    cout << "This program exist for comparing two tree realisation!\n";
 
     int num_of_keys = 0, num_of_qs = 0;
 
@@ -28,23 +37,32 @@ int main()
         cin >> queries[i].frst >> queries[i].scnd;
 
     avl_tree::Tree<int> my_tree;
+
+
     cout << "There is start testing of my tree!\n";
-    vector<int> res_vec2 = Tree_testing(my_tree, keys, queries);
+    vector<int> res_vec2 = Tree_testing(my_tree, keys, queries, option);
 
-    for (auto res : res_vec2)
-        cout << res << " ";
 
-    cout << "\nThere is end testing of my_tree\n\n";
+    if (option == C_KEY)
+    {
+        for (auto res : res_vec2)
+            cout << res << " ";
+
+        cout << "\nThere is end testing of my_tree\n\n";
+    }
 
 
     std::set<int> std_tree;
     cout << "There is start testing of std::set!\n";
-    vector<int> res_vec1 = Tree_testing(std_tree, keys, queries);
+    vector<int> res_vec1 = Tree_testing(std_tree, keys, queries, option);
 
-    for (auto res : res_vec1)
-        cout << res << " ";
+    if (option == C_KEY)
+    {
+        for (auto res : res_vec1)
+            cout << res << " ";
 
-    cout << "\nThere is end testing of std::set\n\n";
+        cout << "\nThere is end testing of std::set\n\n";
+    }
 
 
     //my_tree.Tree_dump(dotname, pngname);
