ea470f459b9684b1223de6c0994f50ca183b1c8c
uslsteen <shurygin0171@gmail.com>
2021-02-28 03:36:14 +0300

    added two new realisation of main:corrective and time

---
+++ Second year/Tree_problem/main_vers/main.cpp
@@ -0,0 +1,79 @@
+
+#include "../test.hpp"
+#include "../Tree.hpp"
+
+
+std::string dotname_orig = "tree_orig.dot";
+std::string pngname_orig = "tree_orig.png";
+
+std::string dotname_cpy = "tree_cpy.dot";
+std::string pngname_cpy = "tree_cpy.png";
+
+int main(int argc, char** argv)
+{
+    if (argc < 2)
+    {
+        std::cerr << "USAGE:" << argv[0] << "-C || -T || -D\n";
+        return 0;
+    }
+
+    opt_t option = Get_options(argc, argv);
+
+    cout << "This program exist for comparing two tree realisation!\n\n";
+
+    int num_of_keys = 0, num_of_qs = 0;
+
+    vector<int> keys;
+    vector<queries_t> queries;
+
+    cin >> num_of_keys;
+    keys.resize(num_of_keys);
+
+    for (int i = 0; i < num_of_keys; ++i)
+        cin >> keys[i];
+
+    cin >> num_of_qs;
+    queries.resize(num_of_qs);
+
+    for (int i = 0; i < num_of_qs; ++i)
+        cin >> queries[i].frst >> queries[i].scnd;
+
+    avl_tree::Tree<int> my_tree;
+
+    cout << "There is start testing of my tree!\n";
+    vector<int> res_vec2 = Tree_testing(my_tree, keys, queries, option, FULL);
+
+
+    if (option == C_KEY)
+    {
+        for (auto& res : res_vec2)
+            cout << res << " ";
+
+        cout << "\nThere is end testing of my_tree\n\n";
+    }
+
+#if 0
+    avl_tree::Tree<int> cpy_my_tree(my_tree);
+#endif
+
+    std::set<int> std_tree;
+    cout << "There is start testing of std::set!\n";
+    vector<int> res_vec1 = Tree_testing(std_tree, keys, queries, option, FULL);
+
+    if (option == C_KEY)
+    {
+        for (auto& res : res_vec1)
+            cout << res << " ";
+
+        cout << "\nThere is end testing of std::set\n\n";
+    }
+
+    if (option == D_KEY)
+    {
+        my_tree.Tree_dump(dotname_orig, pngname_orig);
+        cpy_my_tree.Tree_dump(dotname_cpy, pngname_cpy);
+    }
+
+
+    return 0;
+}
